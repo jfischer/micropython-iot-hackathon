272f245c3a4423627ddcdceacdcba997c3c3e74d
Jeff Fischer <jfischer@us.fujitsu.com>
2017-08-07 22:51:14 -0700

    Download drivers as part of zip file build; for wheels, disable binaries (to avoid python version dependencies)

--- make_zip.sh
+++ make_zip.sh
@@ -11,12 +11,15 @@ cd $TMP_PATH
 #Find pip
 PIP36=`which pip3.6`
 PIP3=`which pip3`
+PIPBARE=`which pip`
 if [[ "$PIP36" != "" ]]; then
   PIP=$PIP36
 elif [[ "$PIP3" != "" ]]; then
   PIP=$PIP3
+elif [[ "$PIPBARE" != "" ]]; then
+  PIP=$PIPBARE
 else
-  echo "Could not find either pip3 or pip3.6"
+  echo "Could not find pip3.6, pip3, or pip"
   exit 1
 fi
 echo "Using $PIP as pip program"
@@ -28,7 +31,7 @@ fi
 set -e  # Stop if there is any error
 
 # Get tool wheels
-$PIP wheel -w micropython-iot-software/python-tools esptool adafruit-ampy \
+$PIP wheel -w micropython-iot-software/python-tools --no-binary=:all: esptool adafruit-ampy \
     wheel paho_mqtt thingflow git+https://github.com/wendlers/mpfshell
 
 # Get firmware
@@ -45,6 +48,12 @@ rm -rf thingflow-python/.git
 mkdir terminal; cd terminal
 wget https://the.earth.li/~sgtatham/putty/latest/w32/putty-0.70-installer.msi
 wget ftp.us.debian.org/debian/pool/main/s/screen/screen_4.5.0-6_amd64.deb
+cd ..
+
+# Download drivers
+mkdir drivers; cd drivers
+wget https://www.silabs.com/documents/public/software/CP210x_Windows_Drivers.zip
+wget https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip
 cd ..
 
 # Prepare esp8266 modules to upload
